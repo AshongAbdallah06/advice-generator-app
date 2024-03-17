53a6d6e4400b7785861ad42275aac1006519290b
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-03-17 15:45:32 +0200

    Complete project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useState } from 'react';
 import './App.css';
+import Axios from "axios";
+import dice from "./images/icon-dice.svg"
+import desktopDivider from "./images/pattern-divider-desktop.svg"
 
 function App() {
-  return (
-    <div className="App">
+    const [adviceNumber, setAdviceNumber] = useState("");
+    const [advice, setAdvice] = useState("");
 
-    </div>
-  );
+
+    const getAdvice = () => {
+        Axios.get("https://api.adviceslip.com/advice").then((res) => {
+            setAdviceNumber(res.data.slip.id);
+            setAdvice(res.data.slip.advice);
+        })
+    }
+    getAdvice();
+
+    const handleClick = () => {
+        getAdvice();
+    }
+    return (
+        <div className="App">
+            <p className="advice-number">ADVICE #{adviceNumber}</p>
+            <blockquote className="advice">{advice}</blockquote>
+            <div className='divider-cont'>
+                <img className="divider" src={desktopDivider} alt="" />
+            </div>
+            <button onClick={handleClick}>
+                <img src={dice} alt="dice-image" className='dice' />
+            </button>
+        </div>
+    );
 }
 
 export default App;
